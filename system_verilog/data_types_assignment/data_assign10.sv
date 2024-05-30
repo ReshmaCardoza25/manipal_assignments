91db6a60cf63f92eba0493d35bdac5e804151aba
Reshma Cardoza <reshmacardoza@FEserver.mirafra.com>
2024-05-30 09:22:05 +0530

    Assignment

--- system_verilog/data_types_assignment/data_assign10.sv
+++ system_verilog/data_types_assignment/data_assign10.sv
@@ -6,7 +6,7 @@ Date:   	21st May 2022
 Version:	1.0
 Description: Concept of splitting of dynamic array using inbuilt methods
 ***************************************************************************/
-_assign10 (); 
+module data_assign10 (); 
 
   // Example: Split the dyn_arr_b[0]=ffffffff  to 
   // dyn_arr_a[0]=ff, dyn_arr_a[1]=ff , dyn_arr_a[2]=ff, dyn_arr_a[3]=ff.
